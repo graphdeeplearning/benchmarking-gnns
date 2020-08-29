78dd40be907bb05ae0f2aea30ff14782cc86014e
Quan Gan <coin2028@hotmail.com>
2020-08-29 06:54:18 +0000

    add script

---
+++ data/script_download_all_datasets_dgl0.5.sh
@@ -0,0 +1,124 @@
+
+
+# Command to download dataset:
+#   bash script_download_all_datasets.sh
+
+
+
+############
+# ZINC
+############
+
+DIR=molecules/
+cd $DIR
+
+FILE=ZINC.pkl
+if test -f "$FILE"; then
+	echo -e "$FILE already downloaded."
+else
+	echo -e "\ndownloading $FILE..."
+	curl https://data.dgl.ai/dataset/benchmarking-gnns/ZINC.pkl -o ZINC.pkl -J -L -k
+fi
+
+cd ..
+
+
+############
+# MNIST and CIFAR10
+############
+
+DIR=superpixels/
+cd $DIR
+
+FILE=MNIST.pkl
+if test -f "$FILE"; then
+	echo -e "$FILE already downloaded."
+else
+	echo -e "\ndownloading $FILE..."
+	curl https://data.dgl.ai/dataset/benchmarking-gnns/MNIST.pkl -o MNIST.pkl -J -L -k
+fi
+
+FILE=CIFAR10.pkl
+if test -f "$FILE"; then
+	echo -e "$FILE already downloaded."
+else
+	echo -e "\ndownloading $FILE..."
+	curl https://data.dgl.ai/dataset/benchmarking-gnns/CIFAR10.pkl -o CIFAR10.pkl -J -L -k
+fi
+
+cd ..
+
+
+############
+# PATTERN and CLUSTER 
+############
+
+DIR=SBMs/
+cd $DIR
+
+FILE=SBM_CLUSTER.pkl
+if test -f "$FILE"; then
+	echo -e "$FILE already downloaded."
+else
+	echo -e "\ndownloading $FILE..."
+	curl https://data.dgl.ai/dataset/benchmarking-gnns/SBM_CLUSTER.pkl -o SBM_CLUSTER.pkl -J -L -k
+fi
+
+FILE=SBM_PATTERN.pkl
+if test -f "$FILE"; then
+	echo -e "$FILE already downloaded."
+else
+	echo -e "\ndownloading $FILE..."
+	curl https://data.dgl.ai/dataset/benchmarking-gnns/SBM_PATTERN.pkl -o SBM_PATTERN.pkl -J -L -k
+fi
+
+cd ..
+
+
+############
+# TSP 
+############
+
+DIR=TSP/
+cd $DIR
+
+FILE=TSP.pkl
+if test -f "$FILE"; then
+	echo -e "$FILE already downloaded."
+else
+	echo -e "\ndownloading $FILE..."
+	curl https://data.dgl.ai/dataset/benchmarking-gnns/TSP.pkl -o TSP.pkl -J -L -k
+fi
+
+cd ..
+
+
+############
+# CSL 
+############
+
+DIR=CSL/
+cd $DIR
+
+FILE=CSL.zip
+if test -f "$FILE"; then
+	echo -e "$FILE already downloaded."
+else
+	echo -e "\ndownloading $FILE..."
+	curl https://www.dropbox.com/s/rnbkp5ubgk82ocu/CSL.zip?dl=1 -o CSL.zip -J -L -k
+	unzip CSL.zip -d ./
+	rm -r __MACOSX/
+fi
+
+cd ..
+
+
+
+
+
+
+
+
+
+
+
