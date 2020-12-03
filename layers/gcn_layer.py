089fb5d108c223e11320e7b36e21a6140491539c
Quan (Andy) Gan <coin2028@hotmail.com>
2020-12-03 13:57:38 +0800

    use builtin reducer (#53)

--- layers/gcn_layer.py
+++ layers/gcn_layer.py
@@ -15,10 +15,7 @@ from dgl.nn.pytorch import GraphConv
 # Sends a message of node feature h
 # Equivalent to => return {'m': edges.src['h']}
 msg = fn.copy_src(src='h', out='m')
-
-def reduce(nodes):
-    accum = torch.mean(nodes.mailbox['m'], 1)
-    return {'h': accum}
+reduce = fn.mean('m', 'h')
 
 class NodeApplyModule(nn.Module):
     # Update node feature h_v with (Wh_v+b)
